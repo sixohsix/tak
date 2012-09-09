605992e13ffe3758c98bbc9e6b5fec542ff3b0b3
Mike Verdone <mike.verdone@gmail.com>
2012-09-09 13:28:16 +0200

    More moving.

--- src/Tecs/Editor.hs
+++ src/Tecs/Editor.hs
@@ -13,6 +13,7 @@ import Tecs.Buffer
 import Tecs.Display
 import Tecs.Editor.Cursor
 import Tecs.Editor.Undo as Undo
+import Tecs.Editor.Edit
 
 import Control.Monad.State
 
@@ -24,50 +25,6 @@ instance Editor SimpleEditor where
     renderBuffer Crop displayedBuffer height width
     setCursor (screenPos editor)
   respond editor evt = (lookupWithDefault evtMap evt) evt editor
-
-insertChar :: Char -> SimpleEditor -> SimpleEditor
-insertChar c st =
-  let cursor = insertPos st
-      buf = buffer st
-  in (pushUndo st) { buffer = insertCharIntoBuffer buf cursor c,
-                     cursorPos = cursor { row = (row cursor) + 1 } }
-
-insertTab :: SimpleEditor -> SimpleEditor
-insertTab st =
-  let insertSpace = insertChar ' '
-  in insertSpace $ insertSpace st
-
-deleteChar :: SimpleEditor -> SimpleEditor
-deleteChar st =
-  let cursor = insertPos st
-      l = line cursor
-      r = row cursor
-      buf = buffer st
-      st' = pushUndo st
-  in if r == 0 && l > 0
-     then let concattedBuf = concatLine buf (line cursor)
-              cursRow = length (lineAt (l - 1) concattedBuf)
-          in st' { buffer = concattedBuf,
-                   cursorPos = Pos (l - 1) cursRow }
-     else st' { buffer = deleteCharFromBuffer buf cursor,
-                cursorPos = cursor { row = (row cursor) - 1 } }
-
-insertLinebreak :: SimpleEditor -> SimpleEditor
-insertLinebreak st =
-  let cursor = insertPos st
-      buf = buffer st
-  in fixScroll $ (pushUndo st) {
-    buffer = insertLinebreakIntoBuffer buf cursor,
-    cursorPos = Pos { line = (line cursor) + 1,
-                      row = 0 } }
-
-killLine :: SimpleEditor -> SimpleEditor
-killLine st =
-  let buf = buffer st
-      l = (line $ insertPos st)
-  in (pushUndo st) {
-    buffer = deleteLine buf l
-    }
 
 ignoreEvt :: (SimpleEditor -> SimpleEditor) -> Event -> SimpleEditor -> SimpleEditor
 ignoreEvt f evt ed = f ed
---
+++ src/Tecs/Editor/Edit.hs
@@ -0,0 +1,52 @@
+module Tecs.Editor.Edit where
+
+import Tecs.Types
+import Tecs.Buffer
+import Tecs.Editor.Cursor
+import Tecs.Editor.Undo
+
+
+insertChar :: Char -> SimpleEditor -> SimpleEditor
+insertChar c st =
+  let cursor = insertPos st
+      buf = buffer st
+  in (pushUndo st) { buffer = insertCharIntoBuffer buf cursor c,
+                     cursorPos = cursor { row = (row cursor) + 1 } }
+
+insertTab :: SimpleEditor -> SimpleEditor
+insertTab st =
+  let insertSpace = insertChar ' '
+  in insertSpace $ insertSpace st
+
+deleteChar :: SimpleEditor -> SimpleEditor
+deleteChar st =
+  let cursor = insertPos st
+      l = line cursor
+      r = row cursor
+      buf = buffer st
+      st' = pushUndo st
+  in if r == 0 && l > 0
+     then let concattedBuf = concatLine buf (line cursor)
+              cursRow = length (lineAt (l - 1) concattedBuf)
+          in st' { buffer = concattedBuf,
+                   cursorPos = Pos (l - 1) cursRow }
+     else st' { buffer = deleteCharFromBuffer buf cursor,
+                cursorPos = cursor { row = (row cursor) - 1 } }
+
+insertLinebreak :: SimpleEditor -> SimpleEditor
+insertLinebreak st =
+  let cursor = insertPos st
+      buf = buffer st
+  in fixScroll $ (pushUndo st) {
+    buffer = insertLinebreakIntoBuffer buf cursor,
+    cursorPos = Pos { line = (line cursor) + 1,
+                      row = 0 } }
+
+killLine :: SimpleEditor -> SimpleEditor
+killLine st =
+  let buf = buffer st
+      l = (line $ insertPos st)
+  in (pushUndo st) {
+    buffer = deleteLine buf l
+    }
+
